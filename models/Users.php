bb90c502175c806fda14e59a10b670809776b598
Victor Aluko <victor.aluko@gmail.com>
2017-01-19 22:47:58 +0200

    Implemented the validation of the name field

--- models/Users.php
+++ models/Users.php
@@ -10,4 +10,12 @@ class Users extends ActiveRecord
     public function getOrders() {
         return $this->hasMany(Orders::className(), ['user_id' =>'id']);
     }
+
+    public function rules()
+    {
+        return [
+            ['name', 'required', 'message' => 'Name must be provided.'],
+            ['name', 'unique'],
+        ];
+    }
 }
