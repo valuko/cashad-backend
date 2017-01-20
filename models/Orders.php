56ae2947153d6b9e092232741306bb25e52eec4f
Victor Aluko <victor.aluko@gmail.com>
2017-01-20 05:39:39 +0200

    Added the behaviour to autogenerate the date when inserting

--- models/Orders.php
+++ models/Orders.php
@@ -8,6 +8,7 @@
 
 namespace app\models;
 
+use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
 
 class Orders extends ActiveRecord
@@ -26,12 +27,25 @@ class Orders extends ActiveRecord
     public function rules()
     {
         return [
-            [['quantity','date','user_id','product_id'], 'required',],
+            [['quantity','user_id','product_id'], 'required',],
         ];
     }
 
     public function extraFields()
     {
         return ['product','user'];
+    }
+
+    public function behaviors()
+    {
+        return [
+            'timestamp' => [
+                'class' => TimestampBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => 'date',
+                ],
+                'value' => function() { return date('Y-m-d');  },
+            ],
+        ];
     }
 }
