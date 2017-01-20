75936bad5245be9e32e929a98555dd5967876d6d
Victor Aluko <victor.aluko@gmail.com>
2017-01-20 05:13:54 +0200

    Implemented Cors correctly

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -8,9 +8,38 @@
 
 namespace app\controllers;
 
+use yii\filters\Cors;
 use yii\rest\ActiveController;
 
 class OrderController extends ActiveController
 {
     public $modelClass = 'app\models\Orders';
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return array_merge(parent::behaviors(), [
+
+            // For cross-domain AJAX request
+            'corsFilter'  => [
+                'class' => Cors::className(),
+                'cors' => [
+                    // restrict access to
+                    'Origin' => ['*'],
+                    'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                    // Allow only POST and PUT methods
+                    'Access-Control-Request-Headers' => ['*'],
+                    // Allow only headers 'X-Wsse'
+                    'Access-Control-Allow-Credentials' => true,
+                    // Allow OPTIONS caching
+                    'Access-Control-Max-Age' => 86400,
+                    // Allow the X-Pagination-Current-Page header to be exposed to the browser.
+                    'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
+                ],
+            ],
+
+        ]);
+    }
 }
--- controllers/UserController.php
+++ controllers/UserController.php
@@ -13,4 +13,32 @@ use yii\rest\ActiveController;
 class UserController extends ActiveController
 {
     public $modelClass = 'app\models\Users';
+
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return array_merge(parent::behaviors(), [
+            // For cross-domain AJAX request
+            'corsFilter' => [
+                'class' => \yii\filters\Cors::className(),
+                'cors' => [
+                    // restrict access to
+                    'Origin' => ['*'],
+                    'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                    // Allow only POST and PUT methods
+                    'Access-Control-Request-Headers' => ['*'],
+                    // Allow only headers 'X-Wsse'
+                    'Access-Control-Allow-Credentials' => true,
+                    // Allow OPTIONS caching
+                    'Access-Control-Max-Age' => 86400,
+                    // Allow the X-Pagination-Current-Page header to be exposed to the browser.
+                    'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
+                ],
+            ],
+
+        ]);
+    }
 }
